936857e93505823ed5b409dc4d0f1e5858bde9a9
Amagi <xingzeyu@sjtu.edu.cn>
2021-12-29 21:08:50 +0800

    feat: create IR builder

---
+++ src/LLVMIR/IRBuilder.java
@@ -0,0 +1,136 @@
+package LLVMIR;
+
+import AST.*;
+
+public class IRBuilder implements ASTVisitor {
+
+    @Override
+    public void visit(ASTNode it) {
+
+    }
+
+    @Override
+    public void visit(ProgramNode it) {
+
+    }
+
+    @Override
+    public void visit(CompoundStatementNode it) {
+
+    }
+
+    @Override
+    public void visit(FunctionDefNode it) {
+
+    }
+
+    @Override
+    public void visit(StatementNode it) {
+
+    }
+
+    @Override
+    public void visit(ClassDefNode it) {
+
+    }
+
+    @Override
+    public void visit(ConstructFunctionDefNode it) {
+
+    }
+
+    @Override
+    public void visit(SimpleStatementNode it) {
+
+    }
+
+    @Override
+    public void visit(ExpressionNode it) {
+
+    }
+
+    @Override
+    public void visit(BlockNode it) {
+
+    }
+
+    @Override
+    public void visit(IfNode it) {
+
+    }
+
+    @Override
+    public void visit(WhileNode it) {
+
+    }
+
+    @Override
+    public void visit(ForNode it) {
+
+    }
+
+    @Override
+    public void visit(PostfixExpressionNode it) {
+
+    }
+
+    @Override
+    public void visit(NewExpressionNode it) {
+
+    }
+
+    @Override
+    public void visit(MemberExpressionNode it) {
+
+    }
+
+    @Override
+    public void visit(FuncCallExpressionNode it) {
+
+    }
+
+    @Override
+    public void visit(SubscriptExpressionNode it) {
+
+    }
+
+    @Override
+    public void visit(PrefixExpressionNode it) {
+
+    }
+
+    @Override
+    public void visit(BinaryExpressionNode it) {
+
+    }
+
+    @Override
+    public void visit(SubExpressionNode it) {
+
+    }
+
+    @Override
+    public void visit(ThisExpressionNode it) {
+
+    }
+
+    @Override
+    public void visit(ConstExpressionNode it) {
+
+    }
+
+    @Override
+    public void visit(IdExpressionNode it) {
+
+    }
+
+    @Override
+    public void visit(CreatorNode it) {
+
+    }
+
+    @Override
+    public void visit(VarDefStatementNode it) {
+
+    }
+}
